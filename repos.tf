f31399b88e5f808fa72336b9f1b620f46a0c82fe
Declan Carroll <declan.carroll@cloudbeds.com>
2021-08-23 11:52:23 +0100

    feat: adding new repo from java template

--- repos.tf
+++ repos.tf
@@ -1,3 +1,15 @@
 data "github_repository" "example" {
   full_name = "shortstacked/java-template"
 }
+
+resource "github_repository" "another_repo" { 
+  name        = "java-from-terraform-again" 
+  description = "My awesome codebase" 
+ 
+  visibility = "public" 
+ 
+  template { 
+    owner      = "shortstacked" 
+    repository = "java-template" 
+  } 
+}
